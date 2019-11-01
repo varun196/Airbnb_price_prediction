368c85d82a404249a5ed76672ac0723fda395a3c
Bhargav Deshpande <bhargavdeshpandepro@gmail.com>
2019-11-01 16:20:30 -0400

    Update regression.py

--- Regression/regression.py
+++ Regression/regression.py
@@ -13,28 +13,37 @@ from sklearn.model_selection import (GridSearchCV, KFold, cross_val_score,
                                      train_test_split)
 from sklearn.preprocessing import Imputer, MinMaxScaler, MultiLabelBinarizer
 from sklearn.tree import DecisionTreeRegressor
-import matplotlib.pyplot as plt
 
 data_train, data_test, labels_train, labels_test = None, None, None, None
 
-with open("../pickles/pca_train_test.pkl","rb") as f:
+with open("pickles/pca_train_test.pkl","rb") as f:
     lst = pickle.load(f)
     data_train, data_test, labels_train, labels_test = lst
     
-reg = LinearRegression().fit(data_train,labels_train)
-labels_pred = reg.predict(data_test)
+    reg = LinearRegression().fit(data_train,labels_train)
+    labels_pred = reg.predict(data_test)
 
-labels_test_np = labels_test.to_numpy()
+    labels_test_np = labels_test.to_numpy()
 
-mse = metrics.mean_squared_error(labels_test_np, labels_pred)
-print("MSE",mse)
+    mse = metrics.mean_squared_error(labels_test_np, labels_pred)
 
-data = None
-with open("../pickles/preprocessed_data.pkl","rb") as f:
-    data = pickle.load(f)
-labels = data['log_price']
-mean_label = labels.mean()
-print("Mean deviation: ", mse/mean_label * 100)
+    data = None
+    with open("pickles/preprocessed_data.pkl","rb") as f:
+        data = pickle.load(f)
+    labels = data['log_price']
+    mean_label = labels.mean()
+    print("Mean deviation: ", mse/mean_label * 100)
 
-# for ind in range(len(labels_test_np)):
-#     print(labels_test_np[ind],labels_pred[ind])
+    print("MSE",mse)
+
+    labels_train_pred = reg.predict(data_train)
+
+    train_mse = metrics.mean_squared_error(labels_train.to_numpy(), labels_train_pred)
+    print("Train MSE",train_mse)
+
+
+
+    df=pd.DataFrame({'x': range(len(labels_test_np)), 'test': labels_test_np, 'predicted': labels_pred })
+    plt.plot( 'x', 'test', data=df)
+    plt.plot( 'x', 'predicted', data=df)
+    plt.show()
