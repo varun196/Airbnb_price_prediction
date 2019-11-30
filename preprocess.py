c8e2a0eaea1f8491db9056742089343019838ba3
Bhargav Deshpande <bhargavdeshpandepro@gmail.com>
2019-11-30 15:43:54 -0500

    Moving code to separate file

--- preprocess.py
+++ preprocess.py
@@ -53,20 +53,5 @@ data = data.join(pd.DataFrame(mlb.fit_transform(data.pop('amenities')),
 # Fill missing data with medians
 data = data.fillna(data.median())
 
-labels = data['log_price']
-data = data.drop('log_price', 1)
-
-# Normalize data
-data = (data - data.min())/(data.max() - data.min())
-
-print(data.shape)
-cov_matrix = data.cov()
-eigen_values,eigen_vectors = np.linalg.eig(cov_matrix)
-plt.plot(range(len(eigen_values)),eigen_values,alpha=0.7, color = "blue")
-plt.xlabel("Principle Component")
-plt.ylabel("Eigen value")
-plt.title("Eigen Value Plot")
-plt.show()
-
 with open("pickles/preprocessed_data_nn.pkl","wb") as f:
     pickle.dump(data,f)
